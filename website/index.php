162744766b538f04b7cea56845e1c08945b2f9d7
Aelerinya <sarah.stecklinck@gmail.com>
2015-12-19 16:33:23 +0100

    Amélioration page d'acceuil

---
+++ website/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Club manga</title>
+    <link rel="stylesheet" href="style.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    <section id="main-section">
+      <h1>Club manga</h1>
+
+      <p>Bienvenue sur le futur site du club manga du lycée Voltaire.</p>
+    </section>
+  </body>
+</html>
