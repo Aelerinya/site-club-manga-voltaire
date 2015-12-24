5ed17ac1b83f0e71d053724e773fe81ae2ae9f6f
Aelerinya <sarah.stecklinck@gmail.com>
2015-12-24 15:27:52 +0100

    Inscription fonctionnelle
    Gestion des erreurs d'entrées
    Insertion d'un nouvel utilisateur (mot de passe sécurisé)
    Mail d'activation (désactivé)

--- website/registration.php
+++ website/registration.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+$displayWelcomeMsg = false;
+
 $errSamePwd = false;
 $errSameEmail = false;
 $errName = false;
@@ -19,7 +21,7 @@ if (isset($_POST['name']) && isset($_POST['email1']) && isset($_POST['email2']) 
   $errEmail = (preg_match('#^[a-z0-9_.-]+@[a-z0-9_.-]{2,}\.[a-z]{2,4}$#', $_POST['email1'])) ? false : true;
 
   include_once('db.php');
-  $db = dbInit()
+  $db = dbInit();
 
   $answer = $db->prepare('SELECT id FROM members WHERE name = ?');
   $answer->execute(array(htmlentities($_POST['name'])));
@@ -35,10 +37,23 @@ if (isset($_POST['name']) && isset($_POST['email1']) && isset($_POST['email2']) 
     $email = htmlentities($_POST['email1']);
     $pwd = password_hash($_POST['pwd1'], PASSWORD_DEFAULT);
 
-    $insert = $db->prepare("INSERT INTO members (name, email, registration_date, password) VALUES (?, ?, NOW(), ?)");
-    $insert->execute(array($name, $email, $pwd));
+    $insertUser = $db->prepare("INSERT INTO members (name, email, registration_date, password) VALUES (?, ?, NOW(), ?)");
+    $insertUser->execute(array($name, $email, $pwd));
+
+    $id = $db->lastInsertId();
+    $key = md5(uniqid(rand(), true));
+    $activationUrl = "http://exemple.exemple/activation.php?key=$key";
+
+    $insertEmailActivation = $db->prepare("INSERT INTO email_activation VALUES (?, 0, ?)");
+    $insertEmailActivation->execute(array($id, $key));
+
+    /*mail($email, 'Validation de votre adresse email',
+    "Cliquez sur ce lien pour activer votre adresse email.\n$activationUrl");**/
+
+    $displayWelcomeMsg = true;
   }
 }
+
 
  ?>
 <!DOCTYPE html>
@@ -52,26 +67,38 @@ if (isset($_POST['name']) && isset($_POST['email1']) && isset($_POST['email2']) 
     <?php include('includes/nav.php');?>
 
     <section id="main-section">
+
+<?php if ($displayWelcomeMsg)
+{
+ ?>
+      <p>Inscription réussie. <?php /* Activez votre adresse email avec le lien présent dans le mail qui vous a été envoyé. */ ?></p>
+<?php
+}
+else
+{ ?>
       <h2>Inscription</h2>
 
       <form action="registration.php" method="post">
-        <label for="name">Nom :</label><br>
-        <input type="text" name="name"><br>
+        <label for="name">Pseudo :</label><br>
+        <input type="text" name="name" maxlength="20"><?php if ($errName) {echo "<span class=\"error\">Votre pseudo doit faire entre 3 et 20 caractères.</span>";} else if ($errUsedName) {echo "<span class=\"error\">Ce pseudo est déjà utilisé.</span>";}?><br>
 
         <label for="email1">Email :</label><br>
-        <input type="email" name="email1"><br>
+        <input type="email" name="email1" maxlength="255"><?php if ($errEmail) {echo "<span class=\"error\">Votre adresse email n'est pas conforme.</span>";} else if ($errUsedEmail) {echo "<span class=\"error\">Cet adresse email est déjà utilisée.</span>";}?><br>
 
         <label for="email2">Confirmez votre email :</label><br>
-        <input type="email" name="email2"><br>
+        <input type="email" name="email2" maxlength="255" autocomplete="off"><?php if ($errSameEmail) {echo "<span class=\"error\">Les deux adresses emails ne sont pas identiques.</span>";}?><br>
 
         <label for="pwd1">Mot de passe :</label><br>
-        <input type="password" name="pwd1"><br>
+        <input type="password" name="pwd1" maxlength="255"><?php if ($errPwd) {echo "<span class=\"error\">Votre mot de passe doit faire au moins 6 caractères.</span>";}?><br>
 
         <label for="pwd2">Confirmez votre mot de passe :</label><br>
-        <input type="password" name="pwd2"><br>
+        <input type="password" name="pwd2" maxlength="255" autocomplete="off"><?php if ($errSamePwd) {echo "<span class=\"error\">Les deux mots de passe ne sont pas identiques.</span>";}?><br>
 
-        <input type="submit" value="S'inscrire"><input type="reset" value="Annuler">
+        <input type="submit" value="S'inscrire" maxlength="255"><input type="reset" value="Annuler">
       </form>
+<?php
+}
+ ?>
     </section>
   </body>
 </html>
