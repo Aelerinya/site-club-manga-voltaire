1a1f410ebc857bc34db48f74b9aab34c0cc098df
Aelerinya <sarah.stecklinck@gmail.com>
2015-12-31 19:06:10 +0100

    Changement name → pseudo
    Changement de "name" à "pseudo" dans tout le fichier.
    Gestion de l'affichage pour un utilisateur déjà connecté.

--- website/registration.php
+++ website/registration.php
@@ -5,40 +5,40 @@ $displayWelcomeMsg = false;
 
 $errSamePwd = false;
 $errSameEmail = false;
-$errName = false;
+$errPseudo = false;
 $errPwd = false;
 $errEmail = false;
-$errUsedName = false;
+$errUsedPseudo = false;
 $errUsedEmail = false;
 
-if (isset($_POST['name']) && isset($_POST['email1']) && isset($_POST['email2']) && isset($_POST['pwd1']) && isset($_POST['pwd2']))
+if (!isset($_SESSION['connected']) && isset($_POST['pseudo']) && isset($_POST['email1']) && isset($_POST['email2']) && isset($_POST['pwd1']) && isset($_POST['pwd2']))
 {
   $errSamePwd = ($_POST['pwd1'] == $_POST['pwd2']) ? false : true;
   $errSameEmail = ($_POST['email1'] == $_POST['email2']) ? false : true;
 
-  $errName = (strlen($_POST['name']) >= 3 && strlen($_POST['name']) <= 20) ? false : true;
+  $errPseudo = (strlen($_POST['pseudo']) >= 3 && strlen($_POST['pseudo']) <= 20) ? false : true;
   $errPwd = (strlen($_POST['pwd1']) >= 6 && strlen($_POST['pwd1']) <= 255) ? false : true;
   $errEmail = (preg_match('#^[a-z0-9_.-]+@[a-z0-9_.-]{2,}\.[a-z]{2,4}$#', $_POST['email1'])) ? false : true;
 
   include_once('db.php');
   $db = dbInit();
 
-  $answer = $db->prepare('SELECT id FROM members WHERE name = ?');
-  $answer->execute(array(htmlentities($_POST['name'])));
-  $errUsedName = ($answer->fetch()) ? true : false;
+  $answer = $db->prepare('SELECT id FROM members WHERE pseudo = ?');
+  $answer->execute(array(htmlentities($_POST['pseudo'])));
+  $errUsedPseudo = ($answer->fetch()) ? true : false;
 
   $answer = $db->prepare('SELECT id FROM members WHERE email = ?');
   $answer->execute(array(htmlentities($_POST['email1'])));
   $errUsedEmail = ($answer->fetch()) ? true : false;
 
-  if (!$errSamePwd && !$errSameEmail && !$errName && !$errPwd && !$errEmail && !$errUsedName && !$errUsedEmail)
+  if (!$errSamePwd && !$errSameEmail && !$errPseudo && !$errPwd && !$errEmail && !$errUsedPseudo && !$errUsedEmail)
   {
-    $name = htmlentities($_POST['name']);
+    $pseudo = htmlentities($_POST['pseudo']);
     $email = htmlentities($_POST['email1']);
     $pwd = password_hash($_POST['pwd1'], PASSWORD_DEFAULT);
 
-    $insertUser = $db->prepare("INSERT INTO members (name, email, registration_date, password) VALUES (?, ?, NOW(), ?)");
-    $insertUser->execute(array($name, $email, $pwd));
+    $insertUser = $db->prepare("INSERT INTO members (pseudo, email, registration_date, password) VALUES (?, ?, NOW(), ?)");
+    $insertUser->execute(array($pseudo, $email, $pwd));
 
     $id = $db->lastInsertId();
     $key = md5(uniqid(rand(), true));
@@ -74,13 +74,19 @@ if (isset($_POST['name']) && isset($_POST['email1']) && isset($_POST['email2']) 
       <p>Inscription réussie. <?php /* Activez votre adresse email avec le lien présent dans le mail qui vous a été envoyé. */ ?></p>
 <?php
 }
+else if (isset($_SESSION['connected']))
+{
+  ?>
+  <p>Vous êtes déjà connecté. Vous n'avez donc pas besoin de vous créer un compte ;)<p>
+  <?php
+}
 else
 { ?>
       <h2>Inscription</h2>
 
       <form action="registration.php" method="post">
-        <label for="name">Pseudo :</label><br>
-        <input type="text" name="name" maxlength="20"><?php if ($errName) {echo "<span class=\"error\">Votre pseudo doit faire entre 3 et 20 caractères.</span>";} else if ($errUsedName) {echo "<span class=\"error\">Ce pseudo est déjà utilisé.</span>";}?><br>
+        <label for="pseudo">Pseudo :</label><br>
+        <input type="text" name="pseudo" maxlength="20"><?php if ($errPseudo) {echo "<span class=\"error\">Votre pseudo doit faire entre 3 et 20 caractères.</span>";} else if ($errUsedPseudo) {echo "<span class=\"error\">Ce pseudo est déjà utilisé.</span>";}?><br>
 
         <label for="email1">Email :</label><br>
         <input type="email" name="email1" maxlength="255"><?php if ($errEmail) {echo "<span class=\"error\">Votre adresse email n'est pas conforme.</span>";} else if ($errUsedEmail) {echo "<span class=\"error\">Cet adresse email est déjà utilisée.</span>";}?><br>
