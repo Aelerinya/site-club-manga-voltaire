b57bde528a07962d07b3227aca90c53fe5580446
Aelerinya <sarah.stecklinck@gmail.com>
2016-01-02 23:52:56 +0100

    Mise à joue nav + autres

--- website/includes/nav.php
+++ website/includes/nav.php
@@ -4,6 +4,15 @@
   <ul>
     <li><a href="index.php">Acceuil</a></li>
     <li><a href="chatbox.php">Chatbox</a></li>
+    <?php if (isset($_SESSION['connected'])) { ?>
+    <li><a href="#" id="logout">Déconnexion</a></li>
+    <form id="logoutForm" action="login.php" method="post">
+      <input type="hidden" name="action" value="logout">
+    </form>
+    <?php } else { ?>
+    <li><a href="login.php">Connexion</a></li>
     <li><a href="registration.php">Inscription</a></li>
+    <?php } ?>
   </ul>
 </nav>
+<script src="script/logout.js" defer></script>
---
+++ website/script/logout.js
@@ -0,0 +1,6 @@
+var logout = document.getElementById("logout");
+var form = document.getElementById("logoutForm");
+
+logout.addEventListener('click', function () {
+  form.submit();
+}, false);
