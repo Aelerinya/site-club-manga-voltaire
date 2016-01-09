c2aeef1d8c5eb74fa5b37c6c2356178ab17bbb36
Aelerinya <sarah.stecklinck@gmail.com>
2016-01-09 21:10:19 +0100

    Ajout redirection erreur 404

---
+++ website/404.php
@@ -0,0 +1,19 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>404, page non trouvée - Club manga Voltaire</title>
+
+    <?php include_once('includes/head.php'); ?>
+  </head>
+  <body>
+    <?php include('includes/nav.php');?>
+
+    <section id="main-section">
+      <h1>Club manga Voltaire</h1>
+
+      <p>Erreur 404 ! La page à laquelle vous avez tenté d'accéder n'existe pas, n'est plus disponible, ou a changé d'adresse. Vous pouvez retourner sur une page du site en cliquant sur un des liens dans le menu.</p>
+    </section>
+  </body>
+</html>
